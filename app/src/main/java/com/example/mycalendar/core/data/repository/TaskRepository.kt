935736498dc671e99a4fc888e664eb806c96cf0e
Nguyen Phuc Son <pson141002@gmail.com>
2024-05-02 11:32:39 +0700

    Refactor repository directory

--- app/src/main/java/com/example/mycalendar/core/data/repository/TaskRepository.kt
+++ app/src/main/java/com/example/mycalendar/core/data/repository/TaskRepository.kt
@@ -1,7 +1,17 @@
 package com.example.mycalendar.core.data.repository
 
 import com.example.mycalendar.core.data.model.Task
+import com.example.mycalendar.core.database.dao.TaskDao
+import com.example.mycalendar.core.database.model.TaskAndUser
+import com.example.mycalendar.core.database.model.toTask
+import javax.inject.Inject
 
 interface TaskRepository {
     suspend fun getAllTasks(): List<Task>
+}
+
+class TaskRepositoryImpl @Inject constructor(private val taskDao: TaskDao): TaskRepository {
+    override suspend fun getAllTasks(): List<Task> {
+        return taskDao.getAllTasks().map(TaskAndUser::toTask)
+    }
 }
