d4b2159690bd8ce98bb458267550bf667e460ba7
Nguyen Phuc Son <pson141002@gmail.com>
2024-05-07 00:06:09 +0700

    Add UI for task schedule editing, working on event and functionalities

--- app/src/main/java/com/example/mycalendar/ui/component/NoDecorationTextField.kt
+++ app/src/main/java/com/example/mycalendar/ui/component/NoDecorationTextField.kt
@@ -2,19 +2,15 @@ package com.example.mycalendar.ui.component
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
